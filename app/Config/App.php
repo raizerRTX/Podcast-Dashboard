5e65a9ad66be47e8348732ee9401d75735541a0d
RaizerRTX <joshuaignacio39@gmail.com>
2023-08-11 18:08:58 +0800

    For production

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,11 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost/PodCast-Dashboard/';
+    // --> For Localhost
+    // public string $baseURL = 'http://localhost/PodCast-Dashboard/';
+
+    // --> For Live Server/Hosting
+    public string $baseURL = 'https://www.cacaopodcast.a2hosted.com/PodCast-Dashboard/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
